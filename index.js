dc608d50b5b6cfb4a296fae842f292fb013f13e4
Michael Winser <mike.winser@gmail.com>
2021-06-07 17:24:11 -0700

    first draft

---
+++ index.js
@@ -0,0 +1,38 @@
+const inputElement = document.getElementById('slider')
+const toggle = document.querySelector('.toggle')
+const nubbin = document.querySelector('.nubbin')
+const pageViews = document.getElementById('pageviews')
+const price = document.getElementById('price')
+const rates = [
+    {pvs: "10K PAGEVIEWS", ppm: 8.00},
+    {pvs: "50K PAGEVIEWS", ppm: 12.00},
+    {pvs: "100K PAGEVIEWS", ppm: 16.00},
+    {pvs: "500K PAGEVIEWS", ppm: 24.00},
+    {pvs: "1M PAGEVIEWS", ppm: 36.00}
+]
+let isYearlyBilling = false
+let isChanging = false
+
+function handleToggle() {
+    isYearlyBilling = !isYearlyBilling
+    nubbin.classList.toggle('shift')
+    toggle.classList.toggle('activated')
+    let rate = rates[inputElement.value].ppm
+    if (isYearlyBilling) rate*=.75
+    price.innerHTML = '$' + rate.toFixed(2)
+}
+
+function handleMove(){
+    if (isChanging){
+        let progress = inputElement.value/(inputElement.max-inputElement.min)*100
+        inputElement.style.setProperty("--webkitProgressPercent", progress + '%')
+        pageViews.innerHTML = rates[inputElement.value].pvs
+        let rate = rates[inputElement.value].ppm
+        if (isYearlyBilling) rate*=.75
+        price.innerHTML = '$' + rate.toFixed(2)
+    }
+}
+inputElement.addEventListener('mousedown', ()=> isChanging=true)
+inputElement.addEventListener('mousemove', ()=> handleMove())
+inputElement.addEventListener('mouseup', ()=> isChanging=false)
+toggle.addEventListener('click', ()=>handleToggle())
